881b17a49a7af2fdb2c002c62cb3f4a83893efbd
Juan Carlos Arroyo Sosa <juan.arroyo@comisiondelaverdad.co>
2021-06-12 07:59:51 -0500

    configuración inicial react con mapbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 4.6097100,
+    longitude: -74.0817500,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/jcarroyos/ckptqtqd31w5s17mwmrykwq07"
+          onViewportChange={viewport => {
+            setViewport(viewport)
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          markers here
+        </ReactMapGL>
     </div>
   );
-}
+} 
+
 
 export default App;

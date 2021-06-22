63c436f4f47cae0a59580c5ab5425bd87f76c9e1
Juan Carlos Arroyo Sosa <juan.arroyo@comisiondelaverdad.co>
2021-06-21 20:48:02 -0500

    mistrar todos los datos en tabs

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { Popup } from "react-map-gl";
+import ReactHtmlParser from 'react-html-parser';
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class Tooltip extends Component {
   render() {
     const { details, handleCloseTooltip } = this.props;
-    //console.log(details.himagenactual[0].formats.thumbnail.url);
+
     return (
       <Popup
         tipSize={0}
@@ -17,31 +21,98 @@ class Tooltip extends Component {
         onClose={() => handleCloseTooltip()}
       >
         <div className="map-tooltip">
-        <div className="container-box">
-          
-          <div className="map-tooltip-field">
-            <div className="map-tooltip-header">{details.htitulo}</div>
-          </div>
-          <div className="map-tooltip-field">
-            <div className="map-tooltip-label">Fecha del hecho:</div>
-            <div className="map-tooltip-value">{details.hfecha}</div>
-          </div>
-          <div className="imagenes">
-            <img alt="imagen-antes" src={details.himagenantes[0].formats.small.url} />
-            <img alt="imagen-despues" src={details.himagenactual[0].formats.small.url} />
-          </div>
-          <div className="map-tooltip-field">
+          <Tabs>
+            <TabList>
+              <Tab>Manifestación</Tab>
+              <Tab>Descripción</Tab>
+              <Tab>Origen</Tab>
+              <Tab>Estado actual</Tab>
+              <Tab>Fuentes</Tab>
+            </TabList>
+            <div className="container-box">
+              {/* Manifestación */}
+              <TabPanel>
+                <div className="map-tooltip-field">
+                  {details.htitulo}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.hfecha}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.participantes[0].pnombre}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.hpregunta}
+                </div>
+                <div className="map-tooltip-img">
+                  <img alt="imagen" src={details.himagenantes[0].formats.small.url} />
+                </div>
+                <div className="map-tooltip-field">
+                  {details.lugares_monumento.lnombrelugar}
+                </div>
+              </TabPanel>
 
-            {details.hdescripcion}
-            {details.hdescripcion}
-            {details.hdescripcion}
+              {/* Descripción */}
+              <TabPanel>
+                <div className="map-tooltip-field">
+                  {details.htitulo}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.hdescripcion}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.participantes[0].pnombre}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.participantes[0].pdescripcion}
+                </div>
+              </TabPanel>
 
-          </div>
-          <div className="map-tooltip-field">
-            <div className="map-tooltip-label">Estado actual:</div>
-            <div className="map-tooltip-value">{details.hestadoactual}</div>
-          </div>
-          </div>
+              {/* Origen */}
+              <TabPanel>
+                <div className="map-tooltip-field">
+                  {details.lugares_monumento.lnombremonumento}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.lugares_monumento.ltipomonumento}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.lugares_monumento.lfechainstalacion}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.lugares_monumento.lmotivoinstalacion}
+                </div>
+              </TabPanel>
+
+              {/* Estado actual */}
+              <TabPanel>
+                <div className="map-tooltip-field">
+                  {details.htitulo}
+                </div>
+                <div className="map-tooltip-field">
+                  {details.hestadoactual}
+                </div>
+                <div className="map-tooltip-img">
+                  <img alt="imagen" src={details.himagenactual[0].formats.small.url} />
+                </div>
+                <div className="map-tooltip-field">
+                  {details.lugares_monumento.lnombrelugar}
+                </div>
+              </TabPanel>
+
+              {/* Fuentes */}
+              <TabPanel>
+                <div className="map-tooltip-field">
+                  {details.htitulo}
+                </div>
+                <div className="map-tooltip-field">
+                  {ReactHtmlParser(details.hfuenteinfo)}
+
+                </div>
+
+              </TabPanel>
+            </div>
+          </Tabs>
         </div>
 
       </Popup>
